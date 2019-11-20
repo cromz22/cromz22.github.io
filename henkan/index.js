7a9d97c97d68d35fde74a65fd861822f0dc1b8df
cromz22 <tt.pfr4343@gmail.com>
2019-11-20 16:09:19 +0900

    add henkan tool

---
+++ henkan/index.js
@@ -0,0 +1,28 @@
+'use strict';
+(function() {
+
+    function toHankaku(str){
+        var hankaku = str.replace(/[!-~]/g,
+          function(s) {
+            return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+          }
+        );
+        return hankaku
+        .replace(/ /g, "　")
+        .replace(/~/g, "～")
+        .replace(/〜/g, "～")
+        .replace(/@/g, "＠");
+      }
+    
+    var hankaku = document.getElementById('hankaku');
+    var zenkaku = document.getElementById('zenkaku');
+    var btn = document.getElementById('btn');
+
+    btn.addEventListener('click', function() {
+        zenkaku.value = toHankaku(hankaku.value);
+    });
+
+    //参考 https://webllica.com/change-double-byte-to-half-width/ 2019年8月9日参照
+
+
+}());
