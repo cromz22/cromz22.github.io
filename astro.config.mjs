37424198536430777ff14db04bfe7ee7689423ea
cromz22 <tt.pfr4343@gmail.com>
2025-07-09 11:19:28 +0900

    prefix default locale true

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ export default defineConfig({
   i18n: {
     locales: ["ja", "en"],
     defaultLocale: "en",
+    fallback: {
+      "ja": "en",
+    },
+    routing: {
+      prefixDefaultLocale: true,
+      fallbackType: "redirect",
+    },
   }
 });
