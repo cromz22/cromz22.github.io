57f554d3e70e1a0d56682242336c7adc69c77445
cromz22 <tt.pfr4343@gmail.com>
2025-08-06 18:22:43 +0900

    try vercel with middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()],
-  i18n: {
-    locales: ["ja", "en"],
-    defaultLocale: "en",
-    fallback: {
-      "ja": "en",
-    },
-    routing: {
-      prefixDefaultLocale: true,
-      fallbackType: "rewrite",
-    },
-  }
 });
