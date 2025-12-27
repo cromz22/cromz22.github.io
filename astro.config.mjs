70c20c5a62375ea164865c1bc469416d5a0e0314
cromz22 <tt.pfr4343@gmail.com>
2025-12-27 21:15:55 +0900

    enable katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   i18n: {
     locales: ["ja", "en"],
     defaultLocale: "en",
